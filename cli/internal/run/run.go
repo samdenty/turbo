884ea6dd5cf9970265a9759bfeaeec4d95c1f573
Mehul Kar <mehul.kar@vercel.com>
2022-10-18 01:15:57 -0700

    everything

--- cli/internal/run/run.go
+++ cli/internal/run/run.go
@@ -205,6 +205,7 @@ func (r *run) run(ctx gocontext.Context, targets []string) error {
 			return err
 		}
 	}
+
 	if ui.IsCI && !r.opts.runOpts.noDaemon {
 		r.base.Logger.Info("skipping turbod since we appear to be in a non-interactive context")
 	} else if !r.opts.runOpts.noDaemon {
@@ -480,6 +481,7 @@ func buildTaskGraphEngine(topoGraph *dag.AcyclicGraph, pipeline fs.Pipeline, rs 
 		topoDeps := make(util.Set)
 		deps := make(util.Set)
 		isPackageTask := util.IsPackageTask(taskName)
+
 		for _, dependency := range taskDefinition.TaskDependencies {
 			if isPackageTask && util.IsPackageTask(dependency) {
 				err := engine.AddDep(dependency, taskName)
@@ -490,24 +492,32 @@ func buildTaskGraphEngine(topoGraph *dag.AcyclicGraph, pipeline fs.Pipeline, rs 
 				deps.Add(dependency)
 			}
 		}
+
 		for _, dependency := range taskDefinition.TopologicalDependencies {
 			topoDeps.Add(dependency)
 		}
+
 		engine.AddTask(&core.Task{
-			Name:     taskName,
-			TopoDeps: topoDeps,
-			Deps:     deps,
+			Name:       taskName,
+			TopoDeps:   topoDeps,
+			Persistent: taskDefinition.Persistent,
+			Deps:       deps,
 		})
 	}
 
-	if err := engine.Prepare(&core.EngineExecutionOptions{
+	engineOptions := &core.EngineExecutionOptions{
 		Packages:  rs.FilteredPkgs.UnsafeListOfStrings(),
 		TaskNames: rs.Targets,
 		TasksOnly: rs.Opts.runOpts.only,
-	}); err != nil {
+	}
+
+	if err := engine.Prepare(engineOptions); err != nil {
 		return nil, err
 	}
 
+	// TaskGraph is a DAG with string references, not the full pipeline.
+	// We need to do some validatio of the TaskGraph _while_ looking at the
+	// information in engine.pipeline, which has the Persistent field.
 	if err := util.ValidateGraph(engine.TaskGraph); err != nil {
 		return nil, fmt.Errorf("Invalid task dependency graph:\n%v", err)
 	}
